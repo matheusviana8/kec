12503232734cf973b7bc65de3fb803e6ea14b8b4
Matheus <matheus.viana28l@gmail.com>
2020-01-15 22:26:02 -0300

    Categoria

---
+++ src/main/java/com/anma/comercial/repository/CategoriaRepository.java
@@ -0,0 +1,9 @@
+package com.anma.comercial.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.anma.comercial.model.Categoria;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+
+}
---
+++ src/main/java/com/anma/comercial/resource/CategoriaResource.java
@@ -0,0 +1,56 @@
+package com.anma.comercial.resource;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.anma.comercial.event.RecursoCriadoEvent;
+import com.anma.comercial.model.Categoria;
+import com.anma.comercial.repository.CategoriaRepository;
+
+@RestController
+@RequestMapping("/categorias")
+public class CategoriaResource {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private ApplicationEventPublisher publisher;
+	
+	@GetMapping
+	@PreAuthorize("hasAuthority('ROLE_PESQUISAR_CATEGORIA') and #oauth2.hasScope('read')")
+	public List<Categoria> listar() {
+		return categoriaRepository.findAll();
+	}
+	
+	@PostMapping
+	@PreAuthorize("hasAuthority('ROLE_CADASTRAR_CATEGORIA') and #oauth2.hasScope('write')")
+	public ResponseEntity<Categoria> criar(@Valid @RequestBody Categoria categoria, HttpServletResponse response) {
+		Categoria categoriaSalva = categoriaRepository.save(categoria);
+		publisher.publishEvent(new RecursoCriadoEvent(this, response, categoriaSalva.getId()));
+		return ResponseEntity.status(HttpStatus.CREATED).body(categoriaSalva);
+	}
+	
+	
+	@GetMapping("/{id}")
+	@PreAuthorize("hasAuthority('ROLE_PESQUISAR_CATEGORIA') and #oauth2.hasScope('read')")
+	public ResponseEntity<Categoria> buscarPeloId(@PathVariable long id) {
+		Categoria categoria = categoriaRepository.findOne(id);
+		return categoria != null ? ResponseEntity.ok(categoria) : ResponseEntity.notFound().build();
+	}
+	
+}
